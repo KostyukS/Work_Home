bb802f01b50869cfec1ec8c4a42069ecb4b51f97
KostyukS <katarsis1234@yandex.ru>
2022-03-28 15:43:53 +0300

    add function

--- Work_Home/netology_work.py
+++ Work_Home/netology_work.py
@@ -6,15 +6,34 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.voto = voto_medio(self)
 
     def rate_hw(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __lt__(self, other):
+        return self.voto < other.voto
+
+    def __str__(self):
+        text = f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания:" \
+               f" \nКурсы в процессе изучения: \nЗавершенные курсы:"
+        return text
+
+
+def voto_medio(obj):
+    lst = []
+    for item in obj.grades:
+        for val in item.values():
+            lst.append(int(val))
+    voto = sum(lst) / len(lst)
+    return voto
 
 
 class Mentor:
@@ -34,7 +53,14 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    self.grades = {}
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+        self.courses_attached = []
+
+    def __str__(self):
+        text = f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: "
+        return text
 
 
 class Reviewer(Mentor):
@@ -46,3 +72,13 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        text = f"Имя: {self.name}\nФамилия: {self.surname}"
+        return text
+
+
+sudent1 = Student('Piter', 'Soren', 'boy')
+student2 = Student('Nicolas', 'Kenis', 'boy')
+student3 = Student('Irina', 'Gray', 'gerl')
+student4 = Student('Sara', 'Conor', 'gerl')
