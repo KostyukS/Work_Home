a996f8f1db161e3c45d31362e4551729196175df
KostyukS <katarsis1234@yandex.ru>
2022-04-07 15:53:33 +0300

    begin

--- Work_Home_1/netology_work_1.py
+++ Work_Home_1/netology_work_1.py
@@ -1,1 +1,16 @@
-print('start')
+def dir_cook(file_obj):
+    keys = ['ingredient_name', 'quantity', 'measure']
+    values = []
+    cook_book = {}
+    with open(file_obj) as file:
+        dish = file.readline()
+        count = file.readline()
+        for i in range(int(count)):
+            values += file.readline().split(' | ')
+            dictionary = dict(zip(keys, values))
+            cook_book[dish] += [dictionary]
+            values = []
+    return cook_book
+
+
+print(dir_cook('file.txt'))
